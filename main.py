2c7d8781aa237e565cd790464c2509bf8ad8c658
myworkshop2019 <1820224503@qq.com>
2025-02-21 16:17:21 +0800

    20250221

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import argparse
 def terminal_args():
     parser = argparse.ArgumentParser(description="Simple example of a training script.")
     parser.add_argument("--test", action="store_true")
-    parser.add_argument("--inference", action="store_true")
+    parser.add_argument("--inference--url="https://cdn11.bigcommerce.com/s-405b0/images/stencil/590x590/products/97/20409/8000-gildan-tee-t-shirt.ca-model__66081.1724276210.jpg"
+", action="store_true")
     return parser.parse_known_args()[0]
 
 
